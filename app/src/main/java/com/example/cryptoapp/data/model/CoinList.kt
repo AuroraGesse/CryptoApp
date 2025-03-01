cace4ccd15778edd0413c5487e099164700a5b98
Aurora Gesse <v.veresk@bk.ru>
2025-03-01 13:55:38 +0800

    create domain layer

--- app/src/main/java/com/example/cryptoapp/data/model/CoinList.kt
+++ app/src/main/java/com/example/cryptoapp/data/model/CoinList.kt
@@ -1,9 +1,9 @@
-package com.example.cryptoapp.pojo
+package com.example.cryptoapp.data.model
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class CoinInfoListOfData (
+data class CoinList (
     @SerializedName("Data")
     @Expose
     val data: List<Datum>? = null
---
+++ app/src/main/java/com/example/cryptoapp/domain/Coin.kt
@@ -0,0 +1,5 @@
+package com.example.cryptoapp.domain
+
+data class Coin(
+    val name: String
+)
